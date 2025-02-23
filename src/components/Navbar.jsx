45ff008986ef0b0d93a3618be01abccd0acecc47
ramohin1236 <mohinr26@gmail.com>
2025-02-23 23:55:18 +0600

    nav mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router'
 
 const Navbar = () => {
 
-  const [isMenuOpen, setIsMenuItemOpen]=useState(false);
-  const [activeDropdown, setActiveDropdown]=useState(null)
+  const [isMenuOpen, setIsMenuItemOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null)
 
-  const toggleMenu =()=>{
-     setIsMenuItemOpen(!isMenuOpen)
+  const toggleMenu = () => {
+    setIsMenuItemOpen(!isMenuOpen)
   }
 
-  const toggleDropdown =(menu)=>{
-   setActiveDropdown(activeDropdown === menu ? null: menu) 
+  const toggleDropdown = (menu) => {
+    setActiveDropdown(activeDropdown === menu ? null : menu)
   }
 
   const menuItems = {
@@ -50,10 +50,10 @@ const Navbar = () => {
     solutions: {
       title: 'Solutions',
       items: [
-        { name: 'For Enterprise', desc: 'Scale your business' },
-        { name: 'For Startups', desc: 'Move faster with CodeTutor' },
-        { name: 'For Agencies', desc: 'Win more clients' },
-        { name: 'For Marketing', desc: 'Drive more growth' },
+        { name: 'Enterprise', desc: 'Scale your business' },
+        { name: ' Startups', desc: 'Move faster with CodeTutor' },
+        { name: 'Agencies', desc: 'Win more clients' },
+        { name: ' Marketing', desc: 'Drive more growth' },
       ]
     },
     resources: {
@@ -68,7 +68,6 @@ const Navbar = () => {
   };
 
 
-
   return (
     <nav className='bg-black text-white fixed top-0 left-0 right-0 border-b border-white/10'>
       <div className='container mx-auto px-4 py-2 sm:px-6 lg:px-8'>
@@ -79,16 +78,42 @@ const Navbar = () => {
           </div>
           {/* dekstop menu */}
           <div className='hidden lg:flex items-center space-x-6'>
-             {
-              Object.keys(menuItems).map((key)=>(
-                <div>
-                    <button onClick={()=>toggleDropdown(key)} className='hover:text-gray-400 cursor-pointer flex items-center px-3 py-2 text-sm rounded-md font-medium'>
-                      {menuItems[key].title}
-                      <MdKeyboardArrowDown className={`ml-2 h-5 w-5 transition-transform ${activeDropdown === key ? 'transform rotate-180': ""}`}/></button>
-                
+            {
+              Object.keys(menuItems).map((key) => (
+                <div className='relative'>
+                  <button onClick={() => toggleDropdown(key)} className='hover:text-gray-400 cursor-pointer flex items-center px-3 py-2 text-sm rounded-md font-medium'>
+                    {menuItems[key].title}
+                    <MdKeyboardArrowDown className={`ml-2 h-5 w-5 transition-transform ${activeDropdown === key ? 'transform rotate-180' : ""}`} /></button>
+
+                  {/* dropdown items */}
+                  {
+                    activeDropdown === key && (
+
+                      <div className='absolute left-0 mt-2 w-screen max-w-md bg-white text-black shadow-lg rounded-md py-1 pl-1 pr-1'>
+                        <div>
+                          {
+                            key === 'platform' ? (<div>Platform name</div>) : <div className='space-y-2 flex flex-col gap-2'>
+                              {menuItems[key].items.map((item, idx) => (
+                                <Link to={`/${key}/${item.name.toLowerCase()}`} className='group flex items-start p-2 rounded-lg hover:bg-gray-300'>
+                                  <div className='px-2'>
+                                     <p className='text-xm font-medium'>{item.name}</p>
+                                     <p className='text-sm text-gray-600 '>{item.desc}</p>
+                                  </div>
+                                </Link>
+                              ))}
+                            </div>
+                          }
+                        </div>
+                      </div>
+
+                    )
+                  }
                 </div>
               ))
-             }
+            }
+
+            <Link to='/enterprice' className='hover:text-gray-400'>Enterprice</Link>
+            <Link to='/pricing' className='hover:text-gray-400'>Pricing</Link>
           </div>
           {/* auth button */}
           <div>auth button</div>

b28c870efa8965cb08965dbc270b9aedf4a3f248
ramohin1236 <mohinr26@gmail.com>
2025-02-26 23:38:34 +0600

    responsive menubar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { MdKeyboardArrowDown } from 'react-icons/md';
+import { MdClose, MdKeyboardArrowDown, MdMenu } from 'react-icons/md';
 import { Link } from 'react-router'
 
 const Navbar = () => {
@@ -89,7 +89,7 @@ const Navbar = () => {
                   {
                     activeDropdown === key && (
 
-                      <div className='absolute left-0 mt-2 w-screen max-w-md bg-white text-black shadow-lg rounded-md py-1 pl-1 pr-1'>
+                      <div className='absolute left-0 mt-2 w-screen max-w-lg bg-white text-black shadow-lg rounded-md py-1 pl-1 pr-1'>
                         <div className='grid md:grid-cols-2 grid-cols-1 gap-12 p-4' >
                           {
                             key === 'platform' ? (menuItems[key]?.sections?.map((section, idx) => (
@@ -102,9 +102,9 @@ const Navbar = () => {
                                       <Link to={`/${key}/${item.name.toLowerCase()}`} className='group flex items-start p-2 rounded-lg hover:bg-gray-300'>
                                         <div className='px-2'>
 
-                                           
 
-                                          <p className='text-xm font-medium text-black'>{item?.name} {item.isNew &&(<span className='ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue.800'>NEW</span>)}</p>
+
+                                          <p className='text-xm font-medium text-black flex items-center'>{item?.name} {item.isNew && (<span className='ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800'>NEW</span>)}</p>
                                           <p className='text-sm text-gray-600 '>{item?.desc}</p>
                                         </div>
                                       </Link>
@@ -136,13 +136,93 @@ const Navbar = () => {
             <Link to='/pricing' className='hover:text-gray-400'>Pricing</Link>
           </div>
           {/* auth button */}
-          <div>auth button</div>
+          <div className='hidden md:flex items-center space-x-4'>
+            <Link to='/login' className='hover:text-gray-300 hidden xl:block'>Log In</Link>
+            <Link to='/contact' className='hover:text-gray-300 hidden xl:block'>Contact</Link>
+            <Link to='/lets-started' className='hover:text-white  bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 '>Get's Started - It's Free</Link>
+          </div>
           {/* mobile menu */}
           <div className='md:hidden'>
-            mobile menu
+            <button onClick={toggleMenu} className='inline-flex items-center p-2 rounded-md hover:text-gray-300 hover:bg-gray-700'>
+              {
+                !isMenuOpen ? (<MdMenu className='block w-6 h-6' />) : (<MdClose className='block w-6 h-6' />)
+              }
+            </button>
           </div>
         </div>
       </div>
+
+      {/* mobile responsive menubar */}
+      {
+        isMenuOpen && (
+          <div className='md:hidden transition-all duration-300 ease-in-out'>
+            <div className='px-2 pt-2 pb-3 space-y-1'>
+              {
+                Object.keys(menuItems).map((key) => (
+                  <div className='space-y-2'>
+                    <button onClick={() => toggleDropdown(key)} className='hover:text-gray-400 cursor-pointer flex items-center px-3 py-2 rounded-md '>
+                      {menuItems[key].title}
+                      <MdKeyboardArrowDown className={`ml-2 h-5 w-5 transition-transform ${activeDropdown === key ? 'transform rotate-180' : ""}`} /></button>
+
+                    {/* mobile dropdown */}
+                    {/* dropdown items */}
+                    {
+                      activeDropdown === key && (
+
+                        <div className='bg-white text-black '>
+                          <div className='pl-4 ' >
+                            {
+                              key === 'platform' ? (menuItems[key]?.sections?.map((section, idx) => (
+                                <div key={idx} className='py-2'>
+                                  <h3 className='text-xs font-semibold text-gray-500 tracking-wider mb-2'>{section?.title}</h3>
+
+                                  <div>
+                                    {
+                                      section.items.map((item, idx) => (
+                                        <Link to={`/${key}/${item.name.toLowerCase()}`} className='group flex items-start p-2 rounded-lg hover:bg-gray-300'>
+                                          <div className='px-2'>
+
+
+
+                                            <p className='text-xm font-medium text-black flex items-center'>{item?.name} {item.isNew && (<span className='ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800'>NEW</span>)}</p>
+                                           
+                                          </div>
+                                        </Link>
+                                      ))
+                                    }
+
+                                  </div>
+                                </div>))) : <div className='space-y-2 flex flex-col gap-2'>
+                                {menuItems[key].items.map((item, idx) => (
+                                  <Link to={`/${key}/${item.name.toLowerCase()}`} className='group flex items-start p-2 rounded-lg hover:bg-gray-300'>
+                                    <div className='px-2'>
+                                      <p className='text-xm font-medium'>{item.name}</p>
+                                      <p className='text-sm text-gray-600 '>{item.desc}</p>
+                                    </div>
+                                  </Link>
+                                ))}
+                              </div>
+                            }
+                          </div>
+                        </div>
+
+                      )
+                    }
+
+                  </div>
+
+                ))
+              }
+              <Link to='/interprices' className='block px-3 py-2 hover:bg-gray-700'>Enterprices</Link>
+              <Link to='/interprices' className='block px-3 py-2 hover:bg-gray-700'>Pricing</Link>
+              <Link to='/interprices' className='block px-3 py-2 hover:bg-gray-700'>Contact Sales</Link>
+              <Link to='/interprices' className='block px-3 py-2 hover:bg-gray-700'>Get Started - It's free</Link>
+              <Link to='/interprices' className='block px-3 py-2 bg-blue-600 hover:bg-blue-700'>Login In</Link>
+
+            </div>
+          </div>
+        )
+      }
     </nav>
   )
 }
